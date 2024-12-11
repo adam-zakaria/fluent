a41df60f670664a4efdb6d630ae4e6b387c11e40
Adam Z <adam.p.zakaria@gmail.com>
2024-12-11 01:14:08 +0000

    save styles

--- deploy_all.sh
+++ deploy_all.sh
@@ -5,6 +5,8 @@
 # There is no --mode for 'vite build', it assumes --mode production, which uses .env.production
 
 # dev
+# frontend at 5174
+# backend at 3001
 pm2 start 'npm run aws_dev' --name fluent_vite_dev && pm2 start 'python3 -m flask run --host=0.0.0.0 --port=3001 --debug' --name fluent_flask_dev
 
 # Prod
